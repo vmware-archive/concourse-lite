ceb1abea16182eb3453945641301471e7c8bebde
Alex Suraci <suraci.alex@gmail.com>
2015-07-12 12:45:13 -0700

    clean up garden after building
    this runs while garden is still up, but that seems to be OK

---
+++ scripts/clean-up-garden.sh
@@ -0,0 +1,4 @@
+umount /var/vcap/data/garden/btrfs_graph/btrfs
+umount /var/vcap/data/garden/btrfs_graph
+losetup -d $(losetup -j /var/vcap/data/garden/garden_graph_backing_store | cut -d: -f1)
+rm -rf /var/vcap/data/garden
