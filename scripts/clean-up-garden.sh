a97ba08f8c7c3dfae03c080df823801b5c9f2f27
Chris Brown <xoebus@xoeb.us>
2015-08-24 10:32:53 -0700

    why is this mounted and why won't this other thing unmount?

--- scripts/clean-up-garden.sh
+++ scripts/clean-up-garden.sh
@@ -1,4 +1,5 @@
-umount /var/vcap/data/garden/btrfs_graph/btrfs
+mount
+#umount /var/vcap/data/garden/btrfs_graph/btrfs
 umount /var/vcap/data/garden/btrfs_graph
 losetup -d $(losetup -j /var/vcap/data/garden/garden_graph_backing_store | cut -d: -f1)
 rm -rf /var/vcap/data/garden
